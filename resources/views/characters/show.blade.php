c5d27aebba64de8f157555f54de67f11ca9a9b25
Christian-Lepidi <christianlepidi@gmail.com>
2024-03-27 17:37:07 +0100

    feat: show

---
+++ resources/views/characters/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('main-content')
+<section class="container mt-5">
+        <h1 class="text-light text-center">{{$character->name}}</h1>
+
+        <a href="{{ route('characters.edit', $character) }}">Modifica</a>
+        <ul>
+            @if (!null($character->description))
+            <li>Descrizione: {{$character->description}}</li>    
+            @endif
+          
+           <li>Attacco: {{$character->attack}}</li>
+           <li>Difesa: {{$character->defense}}</li>
+           <li>Velocità: {{$character->speed}}</li>
+           <li>Vita: {{$character->life}}</li> 
+        </ul>
+   
+
+    </section>
+
+@endsection
