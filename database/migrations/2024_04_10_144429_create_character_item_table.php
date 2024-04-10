7ac2a8362af76e3b8186749e8f1bba3b5bfce4a6
MattiaVolonterio <volonteriomattia@gmail.com>
2024-04-10 17:32:21 +0200

    fix: character_item table

--- database/migrations/2024_04_10_144429_create_character_item_table.php
+++ database/migrations/2024_04_10_144429_create_character_item_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -17,7 +18,6 @@ return new class extends Migration {
             $table->foreignId('character_id')->constrained()->cascadeOnDelete();
             $table->foreignId('item_id')->constrained()->cascadeOnDelete();
             $table->unsignedInteger('quantity')->default(1);
-            $table->timestamps();
         });
     }
 
