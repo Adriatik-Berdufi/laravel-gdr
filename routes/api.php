d1d9703a2bda1067341cac983a00cb60224c54a4
MattiaVolonterio <volonteriomattia@gmail.com>
2024-04-17 15:20:29 +0200

    feat: api and api controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\CharacterController;
+use App\Http\Controllers\Api\ItemController;
+use App\Http\Controllers\Api\TypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::apiResource('characters', CharacterController::class)->only('index', 'show');
+Route::apiResource('items', ItemController::class)->only('index', 'show');
+Route::apiResource('types', TypeController::class)->only('index', 'show');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
